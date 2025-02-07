7556462d05000a0df81334310a392cb8a864da0d
Dilar31 <d.novikov0212@gmail.com>
2025-02-07 14:51:58 +0200

    push HW_1

---
+++ HW_1.js
@@ -0,0 +1,46 @@
+/*HW------1----------*/
+let x = "hello";
+console.log(x);
+let y = "owu";
+console.log(y);
+let z = "ua"
+console.log(z);
+let q=1;
+console.log(q);
+let w=10;
+console.log(w);
+let e = -999;
+console.log(e);
+let r = 123;
+console.log(r);
+let t = 3.14;
+console.log(t);
+let a= 2.7;
+console.log(a);
+let u = 16;
+console.log(u);
+let i=true;
+console.log(i);
+let o= false;
+console.log(o);
+/*-------HW----------1.2*/
+let firstName="Dmitriy";
+let middleName="Alexandroviсh";
+let lastName="Novikov";
+console.log(lastName+" "+firstName+" "+middleName);
+/*_______HW________1.3*/
+let v=100;
+console.log(typeof v);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+/*------additional----------*/
+let s = prompt("Введіть ваше ім'я:");
+let d = prompt("Введіть ваше По-Батькові:")
+let f = prompt("Введіть ваш вік:")
+console.log ("Ім'я та по-батькові:"+s+" "+d+" Вік: "+f);
+alert ("have a nice day");
+let n = typeof q;
+console.log (n);
